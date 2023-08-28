14af87c13e2ce2dedb48d264740c86a02e20b2b2
Gottfrid Olsson <gottfridolsson@hotmail.com>
2023-08-28 16:25:03 +0200

    Read CSV

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import numpy as np
 import CSV_handler as CSV
 
-data = CSV.read()
+# Read CSV #
+data = CSV.read('Mo_Werner_2009_formatted.csv')
+header = CSV.get_header(data)
+wavelength = data[header[0]] # micrometer
+n = data[header[1]]
+k = data[header[2]]
 
+# Calculate dielectric function #
